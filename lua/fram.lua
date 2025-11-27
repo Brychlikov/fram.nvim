788384c237141a70a88cbac429ed6084b0cf969e
Brychlikov <brychlikow@gmail.com>
2025-11-27 11:40:16 +0100

    Update tree-sitter-fram url

--- lua/fram.lua
+++ lua/fram.lua
@@ -13,7 +13,7 @@ function M.setup()
 	local ts_parsers = require("nvim-treesitter.parsers").get_parser_configs()
 	ts_parsers.fram = {
 		install_info = {
-			url = "https://github.com/Brychlikov/tree-sitter-fram",
+			url = "https://github.com/fram-lang/tree-sitter-fram",
 			revision = "09778b3022183a4ff83031830feda26311f0c9ec",
 			files = { "src/parser.c", "src/scanner.c" },
 			branch = "main",
