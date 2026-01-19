37aba14dc5df1172977e9bf88b54db5320f4e6f1
Brychlikov <brychlikow@gmail.com>
2026-01-19 10:19:37 +0100

    try new config

--- lua/fram.lua
+++ lua/fram.lua
@@ -9,18 +9,15 @@ function M.setup()
 		end,
 	})
 
-	-- Register the external tree-sitter grammar
-	local ts_parsers = require("nvim-treesitter.parsers").get_parser_configs()
-	ts_parsers.fram = {
+	-- Register the tree-sitter grammar for :TSInstall support
+	local parsers = require("nvim-treesitter.parsers")
+	parsers.fram = {
 		install_info = {
 			url = "https://github.com/fram-lang/tree-sitter-fram",
 			revision = "09778b3022183a4ff83031830feda26311f0c9ec",
-			files = { "src/parser.c", "src/scanner.c" },
-			branch = "main",
-			generate_requires_npm = false,
-			requires_generate_from_grammar = false,
 		},
 		filetype = "fram",
+		tier = 3,
 	}
 end
 
